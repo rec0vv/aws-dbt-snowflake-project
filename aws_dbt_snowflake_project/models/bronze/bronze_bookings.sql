738151eadb234ee931d014fe730ce8ae0250532e
Aman <aman_ankit@yahoo.com>
2026-04-30 16:28:39 +0530

    today's commit

--- aws_dbt_snowflake_project/models/bronze/bronze_bookings.sql
+++ aws_dbt_snowflake_project/models/bronze/bronze_bookings.sql
@@ -1,1 +1,9 @@
+{% set incremental_flag = 1 %}
+{% set incremental_col = 'CREATED_AT' %}
+
+
 SELECT * FROM {{ source('staging','bookings')}}
+
+{% if incremental_flag == 1 %}
+where {{ incremental_col }} > (SELECT COALESCE(MAX({{ incremental_col }}),'1900-01-01') from {{ this }})
+{% endif %}
